416ff2b00a0e00b368a813d0715b6585e3ccdfff
ken tanimura <>
2017-11-07 00:28:00 +0900

    fix

--- campaign/twitter201711/js/main.js
+++ campaign/twitter201711/js/main.js
@@ -16,5 +16,13 @@
         }, 500);
         return false;
     });
+
+    $('.pull_xvg').on('click',function(){
+        var array = ["よこせ","くれ","ください","ちょうだい",];
+        console.log(array[Math.floor(Math.random() * array.length)]);
+        var random_text = array[Math.floor(Math.random() * array.length)];
+        window.open("https://twitter.com/intent/tweet?text=@vergeliscus%20XVG"+random_text,'_blank');
+        return false;
+    });
 })(jQuery);
 
