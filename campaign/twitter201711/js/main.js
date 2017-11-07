9c4ce49008038594d0d52fc373aa062e83f81e09
ken tanimura <>
2017-11-07 14:38:48 +0900

    fix tweet text

--- campaign/twitter201711/js/main.js
+++ campaign/twitter201711/js/main.js
@@ -21,7 +21,7 @@
         var array = ["よこせ","くれ","ください","ちょうだい",];
         console.log(array[Math.floor(Math.random() * array.length)]);
         var random_text = array[Math.floor(Math.random() * array.length)];
-        window.open("https://twitter.com/intent/tweet?text=@vergeliscus%20XVG"+random_text,'_blank');
+        window.open("https://twitter.com/intent/tweet?text=@vergeliscus%20XVG"+random_text + "  $XVG #VERGE ",'_blank');
         return false;
     });
 })(jQuery);
